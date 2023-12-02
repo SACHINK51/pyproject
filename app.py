b1ebd25677815196c46e0d476bf33ed44d1185ac
SACHINK51 <114231313+SACHINK51@users.noreply.github.com>
2023-12-02 17:12:45 +0000

    updated app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import mysql.connector
 
 mysql = mysql.connector.connect(user='web', password='webPass',
   host='127.0.0.1',
-  database='student')
+  database='SupplyChain')
 
 from logging.config import dictConfig
 
