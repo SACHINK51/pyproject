ba036f8bdb8b46424a5385d7d648c7635f0dea39
harmonycon <61577985+harmonycon@users.noreply.github.com>
2023-12-05 19:30:50 +0000

    Update app.py
    Line 103 corrected the name of the field user_type to userType

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def login():
         user = cursor.fetchone()
         print("Username:", username)
         print("password:", password)
-        print("User Type:", user_type)
+        print("User Type:", userType)
         print("User:", user)
         if user and bcrypt.check_password_hash(user[3], password):
             # Set user information in the session
