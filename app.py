c4c341292a421de8c710f575fb7947597b9249c5
Subhiksharaj08 <77789864+subiksha08@users.noreply.github.com>
2023-12-05 19:42:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def customer_dashboard():
                 'userName': row[6]
             }
             products.append(product)
-        print('products', products)
+            print('products', products)
         return render_template('customer.html', products=products)
     else:
         return 'Access denied. You are not a customer.'
