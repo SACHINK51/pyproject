23fb03a012d3db83a4d25b271596815e8b60fb89
SACHINK51 <114231313+SACHINK51@users.noreply.github.com>
2023-12-11 14:19:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -98,10 +98,6 @@ def login():
         cursor = mysql.cursor(); #create a connection to the SQL instance
         cursor.execute(select_query, (userName, userType))
         user = cursor.fetchone()
-        print("Username:", username)
-        print("password:", password)
-        print("User Type:", user_type)
-        print("User:", user)
         if user and bcrypt.check_password_hash(user[3], password):
             # Set user information in the session
             session['userID'] = user[0]
