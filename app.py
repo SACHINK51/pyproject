99152d7f3ec7c3b63a3faa534c8b04b746cbe407
NaveenNV2303 <138974584+NaveenNV2303@users.noreply.github.com>
2023-12-11 22:06:37 +0000

    test6

--- app.py
+++ app.py
@@ -292,7 +292,7 @@ def filter_method(filter_value):
             }
             products.append(product)
         print('products', products)
-        return render_template('customer.html', products=products)
+        return jsonify(products), 200
     else:
         return 'Access denied. You are not a customer.'
     
